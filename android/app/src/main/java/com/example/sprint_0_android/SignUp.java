cba78f68a8fda5799fbc4d22ddd55983d0c6cf56
Ruiyu Chen <rchen1@epsg.upv.es>
2023-10-31 00:31:53 +0100

    Corrección sobre app registro
    -Se cifra y decifra la contraseña
    
    -El usuario se puede registrar cuando:
    1.No hay campo nulo
    2.No repite el correo en la db
    3.No repite el telefono en la db

--- android/app/src/main/java/com/example/sprint_0_android/SignUp.java
+++ android/app/src/main/java/com/example/sprint_0_android/SignUp.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.security.MessageDigest;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,7 +29,6 @@ public class SignUp extends AppCompatActivity {
     private EditText correo;
     private EditText telefono;
     private EditText contrasenia;
-    private Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +38,16 @@ public class SignUp extends AppCompatActivity {
         this.correo=(EditText) findViewById(R.id.inputCorreo);
         this.telefono=(EditText) findViewById(R.id.inputTelefono);
         this.contrasenia=(EditText) findViewById(R.id.inputContrasenia);
-        this.signUp=(Button) findViewById(R.id.buttonSignUp);
+
     }
     public void boton_registrar(View v){
         //URL de destino para mandar
-        String urlDestino = "http://172.20.10.11/CleanBajoqueta-Cliente/web/bd/registroApp.php";
+        String urlDestino = "http://192.168.1.106/CleanBajoqueta-Cliente/web/bd/registroApp.php";
 
         //Objecto JSON para enviar
         JSONObject postData = new JSONObject();
+
+        byte[] datosCifrados;
 
         //Comprobacion si algún campo esta nulo
         if (nombreApellido.getText().toString().isEmpty()
@@ -52,70 +56,79 @@ public class SignUp extends AppCompatActivity {
                 ||contrasenia.getText().toString().isEmpty())
         {
             //Si algun campo esta nulo, un mensaje de error
-            Log.d(ETIQUETA_LOG, "boton_registrar: algun campo esta nulo");
+            Toast.makeText(getApplicationContext(), "Campo nulo", Toast.LENGTH_SHORT).show();
         }else if (correo.getText().toString().contains("@")==false)
         {
             //Si el correo no lleva "@"
-            Log.d(ETIQUETA_LOG, "boton_registrar: El formato de correo es incorrecto ");
+            Toast.makeText(getApplicationContext(), "El formato de correo es incorrecto", Toast.LENGTH_SHORT).show();
         }else {
             try {
+                datosCifrados = cifra(contrasenia.getText().toString());
+                Log.d(ETIQUETA_LOG, "cifrado = " +datosCifrados);
+
+                String textoDescifrado = descifra(datosCifrados);
+                Log.d(ETIQUETA_LOG, "decifrado = " +textoDescifrado);
+
                 //Si no hay campo nulo, guarda valor en objeto JSON
                 postData.put("nombreApellido", nombreApellido.getText().toString());
                 postData.put("email", correo.getText().toString());
                 postData.put("telefono", formatearTelefono(telefono.getText().toString()));
-                postData.put("contraseña", contrasenia.getText().toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
+                postData.put("contraseña",datosCifrados);
+
+                AndroidNetworking.post(urlDestino)
+                        .addHeaders("Content-Type", "application/json; charset=utf-8")
+                        .addJSONObjectBody(postData)
+                        .setTag("post_data")
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            //El servidor me ha respondido con un Json Object
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                if (response != null && response.length() > 0) {
+                                    try {
+                                        //Leo el mensaje que hay en dentro del response
+                                        String success = response.getString("success");
+                                        String message = response.getString("message");
+
+                                        //Si success me responde con un 1, un toast con el message
+                                        if ("1".equals(success)) {
+                                            Log.d(ETIQUETA_LOG, "Datos guardados correctamente: " + message);
+                                            finish();
+                                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+                                        }
+                                        //Si success me responde con un 0, un toast con el message
+                                        else {
+                                            Log.d(ETIQUETA_LOG, "Datos guardados incorrectamente: " + message);
+                                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Datos guardados incorrectamente", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            @Override
+                            public void onError(ANError error) {
+
+                                if (error != null) {
+                                    Log.d(ETIQUETA_LOG, "Mensaje de error: " + error.getMessage().toString());
+                                }
+                            }
+                        });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                catch (Exception e) {
+                    throw new RuntimeException(e);
             }
         }
-
-        AndroidNetworking.post(urlDestino)
-                .addHeaders("Content-Type", "application/json; charset=utf-8")
-                .addJSONObjectBody(postData)
-                .setTag("post_data")
-                .setPriority(Priority.MEDIUM)
-                .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
-                    //El servidor me ha respondido con un Json Object
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        if (response != null && response.length() > 0) {
-                            try {
-                                //Leo el mensaje que hay en dentro del response
-                                String success = response.getString("success");
-                                String message = response.getString("message");
-
-                                //Si success me responde con un 1, un toast con el message
-                                if ("1".equals(success)) {
-                                    Log.d(ETIQUETA_LOG, "Datos guardados correctamente: " + message);
-                                    finish();
-                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-
-                                }
-                                //Si success me responde con un 0, un toast con el message
-                                else {
-                                    Log.d(ETIQUETA_LOG, "Datos guardados incorrectamente: " + message);
-                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                        } else {
-                            Log.d(ETIQUETA_LOG, "Datos guardados incorrectamente ");
-                        }
-                    }
-
-                    @Override
-                    public void onError(ANError error) {
-
-                        if (error != null) {
-                            Log.d(ETIQUETA_LOG, "Mensaje de error: " + error.getMessage().toString());
-                        }
-                    }
-                });
     }
 
     //Funcion para seguir el formato de telefono
@@ -132,6 +145,34 @@ public class SignUp extends AppCompatActivity {
             // Si el número no coincide con el formato esperado, simplemente devuelve el número original
             return numero;
         }
+    }
+    public byte[] cifra(String sinCifrar) throws Exception {
+        final byte[] bytes = sinCifrar.getBytes("UTF-8");
+        final Cipher aes = obtieneCipher(true);
+        final byte[] cifrado = aes.doFinal(bytes);
+        return cifrado;
+    }
+
+    public String descifra(byte[] cifrado) throws Exception {
+        final Cipher aes = obtieneCipher(false);
+        final byte[] bytes = aes.doFinal(cifrado);
+        final String sinCifrar = new String(bytes, "UTF-8");
+        return sinCifrar;
+    }
+    private Cipher obtieneCipher(boolean paraCifrar) throws Exception {
+        final String frase = "comprobadoyconpermiso";
+        final MessageDigest digest = MessageDigest.getInstance("SHA");
+        digest.update(frase.getBytes("UTF-8"));
+        final SecretKeySpec key = new SecretKeySpec(digest.digest(), 0, 16, "AES");
+
+        final Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        if (paraCifrar) {
+            aes.init(Cipher.ENCRYPT_MODE, key);
+        } else {
+            aes.init(Cipher.DECRYPT_MODE, key);
+        }
+
+        return aes;
     }
 
 }
--- web/bd/registroApp.php
+++ web/bd/registroApp.php
@@ -1,64 +1,74 @@
 <?php
-$conn = mysqli_connect("localhost","root","","bbdd_cleanbajoqueta");
+$conn = mysqli_connect("localhost", "root", "", "bbdd_cleanbajoqueta");
 
 //Si la petición es de metodo POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-   
+
     //Cojo los datos que me ha enviado
     $data = json_decode(file_get_contents("php://input"), true);
 
     //Compruebo que existe datos en dentro y lo guardo en las variables
-    if (isset($data['nombreApellido']) && isset($data['email']) && isset($data['telefono'])&& isset($data['contraseña'])) {
+    if (isset($data['nombreApellido']) && isset($data['email']) && isset($data['telefono']) && isset($data['contraseña'])) {
         $nombreApellido = $data['nombreApellido'];
         $email = $data['email'];
         $telefono = $data['telefono'];
         $contraseña = $data['contraseña'];
-        
-    
-        //Hago que query y sugún el resultado lo respondo con el objecto success
-        $sql1 ="INSERT INTO usuario (`email`, `contraseña`, `nombreApellido`) VALUES ('$email','$contraseña','$nombreApellido')";
-        $sql2 ="INSERT INTO telefono (`email`, `telefono`) VALUES ('$email','$telefono')";
-            if(mysqli_query($conn,$sql1)&&mysqli_query($conn,$sql2)){
-                $result["success"] ="1";
-                $result["message"] ="success";
 
+        //Query para comprobar si tiene correo o telefono registrado dos veces
+        $sqlEmail = "SELECT * FROM usuario WHERE email = '$email'";
+        $comprobacionEmail = mysqli_query($conn, $sqlEmail);
+        $sqlTelefono = "SELECT * FROM telefono WHERE telefono = '$telefono'";
+        $comprobacionTelefono = mysqli_query($conn, $sqlTelefono);
+
+        if ($comprobacionEmail && $comprobacionTelefono) {
+
+            $numRowsEmail = mysqli_num_rows($comprobacionEmail);
+            $numRowsTelefono = mysqli_num_rows($comprobacionTelefono);
+
+            //Si repite, un mensaje de error
+            if ($numRowsEmail > 0 || $numRowsTelefono > 0) {
+
+                switch (true) {
+                    case $numRowsEmail > 0:
+                        $result = ["success" => "0", "message" => "Ya tiene un email registrado"];
+                        break;
+                    case $numRowsTelefono > 0:
+                        $result = ["success" => "0", "message" => "Ya tiene un telefono registrado"];
+                        break;
+                }
+                header('Content-Type: application/json');
+                echo json_encode($result);
+                mysqli_close($conn);
+
+            } else {
+                //Si este correo o telefono no esta registrado, lo inserto
+                $sqlInsertUsuario = "INSERT INTO usuario (`email`, `contraseña`, `nombreApellido`) VALUES ('$email','$contraseña','$nombreApellido')";
+                $sqlInsertTelefono = "INSERT INTO telefono (`email`, `telefono`) VALUES ('$email','$telefono')";
+                if (mysqli_query($conn, $sqlInsertUsuario) && mysqli_query($conn, $sqlInsertTelefono)) {
+
+                    $result = ["success" => "1", "message" => "success"];
+
+                } else {
+                    $result = ["success" => "0", "message" => "error"];
+
+                }
+                header('Content-Type: application/json');
                 echo json_encode($result);
                 mysqli_close($conn);
             }
-            else{
-                $result["success"] ="0";
-                $result["message"] ="error";
+        }
 
-                echo json_encode($result);
-                mysqli_close($conn);
-            }
-       
-
-        error_log("Received POST data: email=$email,contraseña=$contraseña, nombreApellido=$nombreApellido");
-
-        $result = [
-            "success" => "1",
-            "message" => "Datos recibidos y procesados correctamente."
-        ];
-    }    else {
+    } else {
         //Si no existe datos en dentro, respondo con success=0
-        $result = [
-            "success" => "0",
-            "message" => "Datos faltantes en la solicitud."
-        ];
+        $result = ["success" => "0", "message" => "Datos faltantes en la solicitud."];
+        header('Content-Type: application/json');
+        echo json_encode($result);
     }
 
-    
-    header('Content-Type: application/json');
-    echo json_encode($result);
 } else {
-   
-    //Si el metodo no es POST, respondo tambien con success=0 y un mensaje de error
-    $result = [
-        "success" => "0",
-        "message" => "Metodo de solicitud no valido."
-    ];
 
+    //Si el metodo no es POST, respondo tambien con success=0 y un mensaje de error
+    $result = ["success" => "0", "message" => "Metodo de solicitud no valido."];
     header('Content-Type: application/json');
     echo json_encode($result);
 }
