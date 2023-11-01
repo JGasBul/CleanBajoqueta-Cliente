85ec6202b86fa23fd05430b34093379feb5e54f2
Zaida <zaidapastor2002@gmail.com>
2023-11-01 22:24:59 +0100

    Aplicado diseño a login y parte de la lp

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php include("template/cabecera.php"); ?>
                 <div class="jumbotron">
-                    <h1 class="display-3"> Proyecto Biometría </h1>
+                    <h1 class="display-3"> Proyecto Bñoetría </h1>
                     <p class="lead"> Zaida Pastor González </p>
                     <hr class="my-2">
                     <p>Inicie sesión para visualizar los datos</p>
