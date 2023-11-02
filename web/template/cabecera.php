070d5890d5002117600e3106c5e4089a62808559
Zaida <zaidapastor2002@gmail.com>
2023-11-02 17:58:51 +0100

    diseño login y registro aplicados, lp a medias

--- web/template/cabecera.php
+++ web/template/cabecera.php
@@ -8,12 +8,14 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css" />
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <ul class="nav navbar-nav">
+<body >
+    <nav class="navbar navbar-expand-lg bg-body-tertiary  shadow z-1  rounded-bottom" data-bs-theme="light">
+        <ul class="nav navbar-nav ">
 
             <li class="nav-item">
-                <a class="nav-link" href="../index.php">Inicio</a>
+            <a  href="../index.php">
+              <img src="../assets/BlueSky_LogoNofondo.png"  width="40" height="40" class="d-inline-block align-text-top" alt="Logo BlueSky">
+            </a>
             </li>
 
             <li class="nav-item">
@@ -32,47 +34,10 @@
                 <a class="nav-link" href="../tests/test_bd.php">Tests</a>
             </li>
 
+            <li class="nav-item align-items-right">
+              <a  href="../user/login.php">
+              <button type="button" class="btn btn-sm btn-primary fs-5 rounded-pill">Inicie sesión</button>
+              </a>
+            </li>
         </ul>
-    </nav>
-    <nav class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor03">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-          </div>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="search" placeholder="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>   
-    <div class="container">
-        <div class="row">
+</nav>
--- web/user/registroPage.php
+++ web/user/registroPage.php
@@ -18,90 +18,180 @@
 
 <body>
     <br><br><br>
-    <div class="container">
-        <div class="row justify-content-md-center">
-            <div class="col-md-4">
+    <div class="container-fluid">   
+        <div class="row align-items-center">
+            <div class="col-sm">
+                <img src="../assets/registro.jpg"  class="img-fluid" alt="registrophoto">
+            </div>    
+                
+            <div class="col-sm ">
                 <!-- Inicio Card -->
-                <div class="card">
-                    <!-- Inicio Card-Header -->
-                    <header class="card-header text-md-center">Registro</header>
+                <div class="card shadow border-light">
+                     <!-- Inicio Card-Header -->
+                    <div class="card-header text-md-center text-dark border-0 fs-3">
+                        Inicie sesión con su cuenta
+                    </div>
                     <!-- Fin Card-Header -->
-                    <!------------------------------------------------------------------------------------------------------------------->
-                    <!------------------------------------------------------------------------------------------------------------------->
+
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
                     <!-- Inicio Card-Body -->
-                    <div class="card-body">
+                    <div class="card-body ">
                         <!--Inicio Formulario de Registro -->
                         <form method="POST">
                             <!--Inicio Campo Nombre -->
-                            <div class="form-group">
-                                <label> Nombre </label>
-                                <input type="text" class="form-control" name="nombreregistro"
-                                    placeholder="Escribe tu nombre" required>
+                            <div class="text-center">
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-lg-3">
+                                                <span class="align-middle" >
+                                                    <label for="exampleInputEmail1"> Nombre: </label>
+                                                </span>
+                                            </div>
+                                            <div class="col  align-middle">
+                                                    <input type="text" class="form-control  border-0 " name="nombreregistro" placeholder="Escriba su nombre" required>
+                                                    <small id="emailHelp" class="form-text text-muted"> </small>
+                                            </div>
+                                            <div class="col-lg-1  align-middle">
+                                            </div>
+                                        </div>
+                                    </div> 
+                                </span>
+                         
                             </div>
+
                             <!--Fin Campo Nombre -->
-                            <!------------------------------------------------------------------------------------------------------------------->
-                            <!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+
                             <!--Inicio Campo Apellidos -->
-                            <div class="form-group">
-                                <label> Apellidos </label>
-                                <input type="text" class="form-control" name="apellidosregistro"
-                                    placeholder="Escribe tus apellidos" required>
+                            <div class="text-center">
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-lg-3 align-middle">
+                                                <span class="align-middle">
+                                                    <label for="exampleInputEmail1"> Apellidos: </label>
+                                                </span>
+                                            </div>
+
+                                            <div class="col  align-middle">
+                                                    <input type="text" class="form-control  border-0" name="apellidosregistro" placeholder="Escriba sus apellidos" required>
+                                            </div>
+
+                                        </div>
+                                    </div> 
+                                </span>
+                         
                             </div>
                             <!--Fin Campo Apellidos -->
-                            <!------------------------------------------------------------------------------------------------------------------->
-                            <!------------------------------------------------------------------------------------------------------------------->
+
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Contraseña -->
-                            <div class="form-group">
-                                <label>Contraseña </label>
-                                <input type="password" class="form-control" name="contraseniaregistro"
-                                    placeholder="Escribe tu contraseña" required>
+                            <div class="text-center">
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-lg-3 align-middle">
+                                                <span class="align-middle">
+                                                    <label for="exampleInputEmail1"> Contraseña: </label>
+                                                </span>
+                                            </div>
+
+                                            <div class="col  align-middle">
+                                                    <input type="password" class="form-control  border-0" name="contraseniaregistro" placeholder="Escriba su contraseña" required>
+                                            </div>
+
+                                        </div>
+                                    </div> 
+                                </span>
                             </div>
                             <!--Fin Campo Contraseña -->
-                            <!------------------------------------------------------------------------------------------------------------------->
-                            <!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Confirmar Contraseña -->
-                            <div class="form-group">
-                                <label>Confirmar Contraseña </label>
-                                <input type="password" class="form-control" name="confirmarcontraseniaregistro"
-                                    placeholder="Vuelve a escribir tu contraseña" required>
+                            <div class="text-center">
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-lg-3 align-middle">
+                                                <span class="align-middle">
+                                                    <label for="exampleInputEmail1"> Contraseña: </label>
+                                                </span>
+                                            </div>
+
+                                            <div class="col  align-middle">
+                                                    <input type="password" class="form-control  border-0" name="confirmarcontraseniaregistro" placeholder="Vuelva a escribir tu contraseña" required>
+                                            </div>
+
+                                        </div>
+                                    </div> 
+                                </span>
                             </div>
-                            <!--Fin Campo Confirmar Contraseña -->
-                            <!------------------------------------------------------------------------------------------------------------------->
-                            <!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Email -->
-                            <div class="form-group">
-                                <label> Email</label>
-                                <input type="email" class="form-control" name="emailregistro"
-                                    placeholder="email@gmail.com" required>
+                            <div class="text-center">
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-lg-3 align-middle">
+                                                <span class="align-middle">
+                                                    <label for="exampleInputEmail1"> Email: </label>
+                                                </span>
+                                            </div>
+
+                                            <div class="col  align-middle">
+                                                    <input type="password" class="form-control  border-0" name="confirmarcontraseniaregistro" placeholder="Vuelva a escribir tu contraseña" required>
+                                            </div>
+
+                                        </div>
+                                    </div> 
+                                </span>
                             </div>
                             <!--Fin Campo Email -->
-                            <!------------------------------------------------------------------------------------------------------------------->
-                            <!------------------------------------------------------------------------------------------------------------------->
+
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Telefono de Contacto -->
-                            <div class="form-group">
-                                <label> Telefono de Contacto </label>
-                                <input type="tel" class="form-control" name="telefonoregistro"
-                                placeholder="###-##-##-##" pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{2}" required>
-                                <small>Formato: 900-45-67-81</small><br><br>
+                            <div class="text-center">
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                    <div class="container-fluid">
+                                        <div class="row">
+                                            <div class="col-lg-3 align-middle">
+                                                <span class="align-middle">
+                                                    <label for="exampleInputEmail1"> Telefono: </label>
+                                                </span>
+                                            </div>
+
+                                            <div class="col  align-middle">
+                                            <input type="tel" class="form-control" name="telefonoregistro" placeholder="###-##-##-##" pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{2}" required>   
+                                            </div>
+                                        </div>
+                                    </div> 
+                                </span>
+                                <p><small>Formato: 900-45-67-81</small></p>
                             </div>
-                            <!--Fin Campo Telefono de Contacto -->
+                             <!--Fin Campo Telefono de Contacto -->
 
-                            <?php
-                            include("../bd/registro.php");
-                            ?>
+                             <?php include("../bd/registro.php"); ?>
 
-                            <input name="registrar" type="submit" class="btn btn-primary" value="Registrarse">
-                        </form>
+                             </form>
                         <!--Fin Formulario de Registro -->
                     </div>
                     <!-- Fin Card-Body -->
-                    <!------------------------------------------------------------------------------------------------------------------->
-                    <!------------------------------------------------------------------------------------------------------------------->
+                    
+                    <div class="text-center">
+                        <input name="registrar" type="submit" class="btn btn-success fs-5 rounded-pill" value="  Registrarse  ">
+                    </div>
+                    <br><br>
                 </div>
-                <!-- Fin Card -->
             </div>
         </div>
     </div>
+
 
 </body>
 <!-- Fin Body -->
