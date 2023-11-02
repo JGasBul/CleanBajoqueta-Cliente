11649bd172c734b3eee7f10433740a2452735582
Pepe Gascó Bule <pepegascobule@gmail.com>
2023-11-02 21:53:21 +0100

    Registro funciona pero no redirige cuando esta correcto ni detecta cuando recibe un 400

--- web/user/registroPage.php
+++ web/user/registroPage.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-<?php include("../bd/registro.php"); ?>
+
 <head>
     <meta charset="UTF-8">
     <title>Proyecto 3A - Registro</title>
@@ -18,56 +18,56 @@
 
 <body>
     <br><br><br>
-    <div class="container-fluid">   
+    <div class="container-fluid">
         <div class="row align-items-center">
             <div class="col-sm">
-                <img src="../assets/registro.jpg"  class="img-fluid" alt="registrophoto">
-            </div>    
-                
+                <img src="../assets/registro.jpg" class="img-fluid" alt="registrophoto">
+            </div>
+
             <div class="col-sm ">
                 <!-- Inicio Card -->
                 <div class="card shadow border-light">
-                     <!-- Inicio Card-Header -->
+                    <!-- Inicio Card-Header -->
                     <div class="card-header text-md-center text-dark border-0 fs-3">
                         Inicie sesión con su cuenta
                     </div>
                     <!-- Fin Card-Header -->
 
-<!------------------------------------------------------------------------------------------------------------------->
-<!------------------------------------------------------------------------------------------------------------------->
+                    <!------------------------------------------------------------------------------------------------------------------->
+                    <!------------------------------------------------------------------------------------------------------------------->
                     <!-- Inicio Card-Body -->
                     <div class="card-body ">
                         <!--Inicio Formulario de Registro -->
                         <form method="POST">
                             <!--Inicio Campo Nombre -->
                             <div class="text-center">
-                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6">
                                     <div class="container-fluid">
                                         <div class="row">
                                             <div class="col-lg-3">
-                                                <span class="align-middle" >
+                                                <span class="align-middle">
                                                     <label for="exampleInputEmail1"> Nombre: </label>
                                                 </span>
                                             </div>
                                             <div class="col  align-middle">
-                                                    <input type="text" class="form-control  border-0 " name="nombreregistro" placeholder="Escriba su nombre" required>
-                                                    <small id="emailHelp" class="form-text text-muted"> </small>
+                                                <input type="text" class="form-control  border-0 " name="nombreregistro" placeholder="Escriba su nombre" required>
+                                                <small id="emailHelp" class="form-text text-muted"> </small>
                                             </div>
                                             <div class="col-lg-1  align-middle">
                                             </div>
                                         </div>
-                                    </div> 
+                                    </div>
                                 </span>
-                         
+
                             </div>
 
                             <!--Fin Campo Nombre -->
-<!------------------------------------------------------------------------------------------------------------------->
-<!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
 
                             <!--Inicio Campo Apellidos -->
                             <div class="text-center">
-                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6">
                                     <div class="container-fluid">
                                         <div class="row">
                                             <div class="col-lg-3 align-middle">
@@ -77,21 +77,21 @@
                                             </div>
 
                                             <div class="col  align-middle">
-                                                    <input type="text" class="form-control  border-0" name="apellidosregistro" placeholder="Escriba sus apellidos" required>
+                                                <input type="text" class="form-control  border-0" name="apellidosregistro" placeholder="Escriba sus apellidos" required>
                                             </div>
 
                                         </div>
-                                    </div> 
+                                    </div>
                                 </span>
-                         
+
                             </div>
                             <!--Fin Campo Apellidos -->
 
-<!------------------------------------------------------------------------------------------------------------------->
-<!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Contraseña -->
                             <div class="text-center">
-                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6">
                                     <div class="container-fluid">
                                         <div class="row">
                                             <div class="col-lg-3 align-middle">
@@ -101,19 +101,19 @@
                                             </div>
 
                                             <div class="col  align-middle">
-                                                    <input type="password" class="form-control  border-0" name="contraseniaregistro" placeholder="Escriba su contraseña" required>
+                                                <input type="password" class="form-control  border-0" name="contraseniaregistro" placeholder="Escriba su contraseña" required>
                                             </div>
 
                                         </div>
-                                    </div> 
+                                    </div>
                                 </span>
                             </div>
                             <!--Fin Campo Contraseña -->
-<!------------------------------------------------------------------------------------------------------------------->
-<!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Confirmar Contraseña -->
                             <div class="text-center">
-                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6">
                                     <div class="container-fluid">
                                         <div class="row">
                                             <div class="col-lg-3 align-middle">
@@ -123,18 +123,18 @@
                                             </div>
 
                                             <div class="col  align-middle">
-                                                    <input type="password" class="form-control  border-0" name="confirmarcontraseniaregistro" placeholder="Vuelva a escribir tu contraseña" required>
+                                                <input type="password" class="form-control  border-0" name="confirmarcontraseniaregistro" placeholder="Vuelva a escribir tu contraseña" required>
                                             </div>
 
                                         </div>
-                                    </div> 
+                                    </div>
                                 </span>
                             </div>
-<!------------------------------------------------------------------------------------------------------------------->
-<!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Email -->
                             <div class="text-center">
-                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6">
                                     <div class="container-fluid">
                                         <div class="row">
                                             <div class="col-lg-3 align-middle">
@@ -144,20 +144,20 @@
                                             </div>
 
                                             <div class="col  align-middle">
-                                                    <input type="email" class="form-control  border-0" name="emailregistro" placeholder="Escriba su email" required>
+                                                <input type="email" class="form-control  border-0" name="emailregistro" placeholder="Escriba su email" required>
                                             </div>
 
                                         </div>
-                                    </div> 
+                                    </div>
                                 </span>
                             </div>
                             <!--Fin Campo Email -->
 
-<!------------------------------------------------------------------------------------------------------------------->
-<!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
+                            <!------------------------------------------------------------------------------------------------------------------->
                             <!--Inicio Campo Telefono de Contacto -->
                             <div class="text-center">
-                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6"> 
+                                <span class=" text-left badge rounded-pill text-bg-light shadow-lg  p-2 mb-2 w-75 align-items-center fs-6">
                                     <div class="container-fluid">
                                         <div class="row">
                                             <div class="col-lg-3 align-middle">
@@ -167,23 +167,22 @@
                                             </div>
 
                                             <div class="col  align-middle">
-                                            <input type="tel" class="form-control" name="telefonoregistro" placeholder="###-##-##-##" pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{2}" required>   
+                                                <input type="tel" class="form-control" name="telefonoregistro" placeholder="###-##-##-##" pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{2}" required>
                                             </div>
                                         </div>
-                                    </div> 
+                                    </div>
                                 </span>
                                 <p><small>Formato: 900-45-67-81</small></p>
                             </div>
-                             <!--Fin Campo Telefono de Contacto -->
-
-                             </form>
+                            <!--Fin Campo Telefono de Contacto -->
+                            <?php include("../bd/registro.php"); ?>
+                            <div class="text-center">
+                                <input name="registrar" type="submit" class="btn btn-success fs-5 rounded-pill" value="  Registrarse  ">
+                            </div>
+                        </form>
                         <!--Fin Formulario de Registro -->
                     </div>
                     <!-- Fin Card-Body -->
-                    
-                    <div class="text-center">
-                        <input name="registrar" type="submit" class="btn btn-success fs-5 rounded-pill" value="  Registrarse  ">
-                    </div>
                     <br><br>
                 </div>
             </div>
