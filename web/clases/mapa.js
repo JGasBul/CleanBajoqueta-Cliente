80934267006a16dfe937ecdcc2d212e93ca159c3
Arnau <clavileno3000@gmail.com>
2023-12-01 16:16:42 +0100

    Versión Inicial del Mapa en Web
    He hecho una versión inicial del mapa con unas cuantas funciones.
    Además, he implementado una clase nueva llamada "contaminante" para el futuro.
    El actual mapa presenta las siguientes funciones:
    
    - Crear un marcador
    - Crear un area circular
    - Crear un polígono
    - Añadir un popup a cualquiera de los anteriores 3 objetos del mapa
    - Icono que te lleva a tu localización exacta
    - Función para ir a un punto en concreto (el punto está predeterminado dentro de código)
    - Filtro según la capa del contaminante, es decir, activar/desactivar las capas de los contaminantes (ozono y CO por el momento)

---
+++ web/clases/mapa.js
@@ -0,0 +1,184 @@
+//----------------------------------------------------------------------------------------------------------
+// Autor: Arnau Soler Tomás
+// Fichero: mapa.js
+// Descripción: Fichero JS en el que irán todas las funciones relacionadas con el mapa.
+// Nota: Se hace en JS con tal de aprovechar el archivo para la App
+//----------------------------------------------------------------------------------------------------------
+
+// Variable Global
+var map = null;
+
+//----------------------------------------------------------------------------------------------------------
+// String nombreMapa --> map()
+// Descripción: Tiene 2 pasos:
+// 1 paso: Crear un <div> con la id del mapa en el HTML
+// 2 paso: Llamar a esta función pasandole la id del mapa
+//----------------------------------------------------------------------------------------------------------
+function mapa(nombre) {
+
+    map = L.map(nombre);
+    map.setView([0, 0], 1); //Valores: [centro], zoom;
+    L.tileLayer('https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=kIRP9eC6t8HxGCalVgc5', {
+        attribution:
+            '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    }).addTo(map);
+
+    console.log("mapa creado");
+    L.control.locate().addTo(map);
+    console.log("mapa Locate creado");
+
+    console.log("Inicio añadir Contaminantes");
+    res = añadirContaminantes();
+    if(res){
+        var layerControl = L.control.layers({},{"Ozono":res[0],"CO":res[1]}).addTo(map);
+    }
+}
+//----------------------------------------------------------------------------------------------------------
+// Punto: punto --> crearMarcador() --> Marker: marker
+// Descripción: crea un Marcador en base a un punto
+//----------------------------------------------------------------------------------------------------------
+function crearMarker(punto) {
+    if (punto == null) { punto = [0, 0]; }
+    var marker = L.marker(punto).addTo(map);
+    return marker;
+}
+//----------------------------------------------------------------------------------------------------------
+// Punto: punto --> crearCirculo()
+// Descripción: crea un area circular en base a un punto
+//----------------------------------------------------------------------------------------------------------
+function crearCirculo(punto, color) {
+    if (punto == null) {
+        punto = [0, 0];
+
+    }
+
+    var circle = L.circleMarker(punto, {
+        color: color,
+        fillColor: color,
+        fillOpacity: 1,
+        radius: 25
+    });
+
+    return circle
+}
+function añadirContaminantes() {
+    console.log("añadirContaminante inicia");
+    listaContam = [];
+    //contaminante = consulta GET
+    //#region Contaminantes de Prueba
+    contaminante = new Contaminante("ozono", 244, [0, 0]);
+    listaContam.push(contaminante);
+    contaminante = new Contaminante("ozono", 332, [20, 20]);
+    listaContam.push(contaminante);
+    contaminante = new Contaminante("ozono", 423, [0, 20]);
+    listaContam.push(contaminante);
+    contaminante = new Contaminante("ozono", 544, [20, 0]);
+    listaContam.push(contaminante);
+
+    contaminante = new Contaminante("co", 123, [60, 60]);
+    listaContam.push(contaminante);
+    contaminante = new Contaminante("co", 32, [80, 60]);
+    listaContam.push(contaminante);
+    contaminante = new Contaminante("co", 45, [60, 80]);
+    listaContam.push(contaminante);
+
+
+    listaOzono = [];
+    listaCO = [];
+
+    //console.log("Lista Ozono: "+listaOzono);
+    //console.log("Lista CO: "+listaCO);
+    //#endregion
+
+    var layerOzono;
+    var layerCO;
+
+    for (var i = 0; i < listaContam.length; i++) {
+        estadoColor = estadocolor(listaContam[i]);
+
+        contam = crearCirculo([20, 20], estadoColor[1]);
+
+        mensaje = "Tipo: " + listaContam[i].tipo + "<br>Valor: " + listaContam[i].valor + "<br>Estado: " + estadoColor[0];
+
+        //console.log(listaContam);
+
+        añadirPopup(contam, mensaje);
+
+
+        if (listaContam[i].tipo == "ozono") { listaOzono.push(contam); }
+        else if (listaContam[i].tipo == "co") { listaCO.push(contam); }
+        else {
+            console.error("Error al distribuir contaminantes");
+            return false;
+        }
+
+        console.log("Posicion: " + i + ", Tipo: " + listaContam[i].tipo + ", Valor: " + listaContam[i].valor);
+
+        layerOzono = L.layerGroup(listaOzono);
+        layerCO = L.layerGroup(listaCO);
+
+        layerOzono.addTo(map);
+        layerCO.addTo(map);
+    }
+
+    return [layerOzono,layerCO];
+
+}
+
+function estadocolor(cont) {
+    color = "green"; //Verde Hex
+    estado = "Seguro";
+    if (cont.valor >= 250) {
+        color = "yellow";
+        estado = "Regular"
+    }
+    if (cont.valor >= 500) {
+        color = "red";
+        estado = "Peligroso"
+    }
+    return [estado, color];
+}
+//----------------------------------------------------------------------------------------------------------
+// [Punto]: puntos --> crearPoligono()
+// Descripción: crea un Poligono en base a una lista de puntos
+//----------------------------------------------------------------------------------------------------------
+function crearPoligono(puntos) {
+    if (puntos == null) { puntos = [[0, 0], [20, 20], [60, 60]]; }
+    var polygon;
+    polygon = L.polygon(puntos).addTo(map);
+
+}
+//----------------------------------------------------------------------------------------------------------
+// (marker || circle || polygon):item, String:mensaje --> añadirPopup()
+// Descripción: añade un Popup con un mensaje a un item en concreto
+//----------------------------------------------------------------------------------------------------------
+function añadirPopup(item, mensaje) {
+    item.bindPopup(mensaje);
+}
+//----------------------------------------------------------------------------------------------------------
+// Punto:punto, N:zoom --> centrarEn()
+// Descripción: centra el mapa en un punto en concreto con un zoom determinado
+//----------------------------------------------------------------------------------------------------------
+function centrarEn(punto, zoom) {
+    if (punto == null) {
+        //punto = [40.737, -73.923];
+        punto = [0, 0];
+    }
+    if (zoom == null) {
+        zoom = 16;
+    }
+    map.flyTo(punto, zoom); //([Latitud, Longitud], Zoom)
+}
+//----------------------------------------------------------------------------------------------------------
+// Punto:punto, N:zoom --> centrarEn()
+// Descripción: centra el mapa en un punto en concreto con un zoom determinado
+//----------------------------------------------------------------------------------------------------------
+function tuLocalizacion(punto, zoom) {
+    if (punto == null) {
+        punto = [40.737, -73.923];
+    }
+    if (zoom == null) {
+        zoom = 16;
+    }
+    centrarEn(punto, zoom);
+}
---
+++ web/publico/mapa.php
@@ -0,0 +1,131 @@
+<php 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mapa de contaminación</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+
+    <!-- LeafLet -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    
+    <!-- Plugin Leaflet: Leaflet.Locate -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.79.0/dist/L.Control.Locate.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.79.0/dist/L.Control.Locate.min.js" charset="utf-8"></script>
+    
+    <!-- contaminante.js -->
+    <script src="../clases/contaminante.js"></script>
+    <!-- mapa.CSS y mapa.JS -->
+    <link rel="stylesheet" href="../css/mapa.css"/>
+    <script src="../clases/mapa.js"></script>
+</head>
+
+<!-- Inicio Header -->
+<header>
+    <?php include("../template/cabecera.php"); ?>
+</header>
+<!-- Fin Header -->
+<!------------------------------------------------------------------------------------------------------------------------>
+<!------------------------------------------------------------------------------------------------------------------------>
+<!-- Inicio Body -->
+<body>
+    <div class="container-fluid">
+        <div class="row align-items-center">
+
+            <div class="col-sm ">
+                <!-- Inicio Card -->
+                <div class="card shadow border-light">
+                    <!-- Inicio Card-Header -->
+                    <div class="card-header text-md-center text-dark border-0 fs-3">
+                        Mapa de Contaminación
+                    </div>
+                    <!-- Fin Card-Header -->
+
+                    <!------------------------------------------------------------------------------------------------------------------->
+                    <!------------------------------------------------------------------------------------------------------------------->
+                    <!-- Inicio Card-Body -->
+                    <div class="card-body ">
+                        <div id="map"></div>
+
+                        <button type="button" onclick="crearMarker()">Añadir Marcador</button>
+                        <button type="button" onclick="crearCirculo()">Añadir Area Circular</button>
+                        <button type="button" onclick="crearPoligono()">Añadir Polígono</button>
+                        <button type="button" onclick="centrarEn()">CentrarEn</button>
+                    </div>
+                    <!-- Fin Card-Body -->
+                    <br><br>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<!-- Fin Body -->
+<!------------------------------------------------------------------------------------------------------------------------>
+<!------------------------------------------------------------------------------------------------------------------------>
+<!-- Inicio Footer -->
+<footer>
+    <?php include("../template/pie.php"); ?>
+</footer>
+<!-- Fin Footer -->
+
+<script>
+    //Inicializar mapa
+    mapa('map');
+
+//#region Ejemplos de funciones
+    /* Crear mapa
+    const map = L.map('map');
+    map.setView([0, 0], 1); //Valores: [centro], zoom;
+    L.tileLayer('https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=kIRP9eC6t8HxGCalVgc5', {
+        attribution:
+            '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    }).addTo(map);
+    */
+
+
+    /* Añadir un Marcador
+    var marker = L.marker([51.5, -0.09]).addTo(map);
+    */    
+    
+    /*Añadir un area circular
+    var circle = L.circle([51.508, -0.11], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+    */
+
+    /*Añadir un area poligonal
+    var polygon = L.polygon([
+        [51.509, -0.08],
+        [51.503, -0.06],
+        [51.51, -0.047]
+    ]).addTo(map);
+    */
+
+    /*Añadir PopUps al marcador, circulo y polígono
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+    circle.bindPopup("I am a circle.");
+    polygon.bindPopup("I am a polygon.");
+    */
+
+    /*function mapa(nombre){
+    
+    map = L.map(nombre);
+    map.setView([0, 0], 1); //Valores: [centro], zoom;
+    L.tileLayer('https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=kIRP9eC6t8HxGCalVgc5', {
+        attribution:
+            '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    }).addTo(map);
+}*/
+    //#endregion
+</script>
+
+</html>
